dac97c2d13ab900d0b7c6d05bd76bc22cf723128
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-01-21 20:49:09 -0800

    Completed projects controller/view

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,22 @@
+class ProjectsController < ApplicationController
+	def index
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(params[:project]) #<co id="ch03_551_1" />
+		if @project.save
+			redirect_to @project,
+			:notice => "Project has been created."
+		else
+			#nothing yet
+		end
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+end
