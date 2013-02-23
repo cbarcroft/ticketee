9517f2ed5568e5cb2a747951f115b441a7213a6f
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-02-22 16:51:58 -0800

    Make projects visible to users with permission to see them only

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,16 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name
 
+
   has_many :tickets, :dependent => :delete_all
 
-  validates :name, :presence => true
+  validates :name, :presence => true, :uniqueness => true
+
+  has_many :permissions, :as => :thing
+
+  def self.viewable_by(user)
+  	joins(:permissions).where(:permissions => { :action => "view", :user_id => user.id} )
+  end
+
+  scope :admins, where(:admin => true)
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ProjectsController do
 	let(:user){ Factory(:confirmed_user) }
-	let(:project){ mock_model(Project, :id => 1) }
+	let(:project){ Factory(:project) }
 
 	context "Standard users" do
 		before do
@@ -23,12 +23,20 @@ describe ProjectsController do
 				flash[:alert].should == "You must be an admin to do that."
 			end
 		end
+
+		it "cannot access the show action without permission" do
+			sign_in(:user, user)
+			get :show, :id => project.id
+			response.should redirect_to(projects_path)
+			flash[:alert].should == "The project you were looking for could not be found."
+		end
 	end
 
 	it "displays an error for a missing project" do
-    get :show, :id => "not-here"
-    response.should redirect_to(projects_path)
-    message = "The project you were looking for could not be found."
-    flash[:alert].should == message
-  end
+		sign_in(:user, user)
+    	get :show, :id => "not-here"
+    	response.should redirect_to(projects_path)
+    	message = "The project you were looking for could not be found."
+    	flash[:alert].should == message
+  	end
 end
