c52d91bc09c9781d4c4dabcd058e24742543f973
Chris Barcroft <ChrisABarcroft@gmail.com>
2013-02-25 17:01:23 -0800

    Add permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Ticketee::Application.routes.draw do
     :to => 'users#confirmation',
     :as => 'confirm_user'
 
+  put 'admin/users/:user_id/permissions',
+      :to => 'admin/permissions#update',
+      :as => :update_user_permissions
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -29,7 +33,9 @@ Ticketee::Application.routes.draw do
 
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
 
   # Sample resource route with options:
